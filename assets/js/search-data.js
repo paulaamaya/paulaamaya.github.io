a81f7cb8aeac890b5e0014a77b9e45d73d63864b
paulaamaya <dev@paulaamaya.com>
2025-04-25 22:06:11 +0000

    Deploying to gh-pages from @ paulaamaya/paulaamaya.github.io@12d5af13d7bc00b6e032caf9938744ec024a1c5e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This is an abridged version of my CV. For the complete version, please refer to the PDF linked above.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
